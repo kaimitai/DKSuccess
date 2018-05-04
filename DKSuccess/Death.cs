dcdee00e9843e6eb7bce13f614f1c21f933a221e
Kai E. Frøland <kai.einar.o.froland@accenture.com>
2018-05-04 16:08:18 +0200

    Added function to the Death objects to see if they fulfill a given criteria (level range and board type)

--- DKSuccess/Death.cs
+++ DKSuccess/Death.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DKSuccess {
-    class Death : IComparable<Death> {
+namespace DKSuccess
+{
+    class Death : IComparable<Death>
+    {
 
         int Level;
         int Board;
@@ -14,8 +16,7 @@ namespace DKSuccess {
             try {
                 Level = int.Parse(death.Substring(0, 1));
                 Board = int.Parse(death.Substring(2, 1));
-            }
-            catch (Exception ex) {
+            } catch (Exception ex) {
                 throw new Exception("Could not instantiate Death from string " + death + ". Expected form; \"L-B\"" + Environment.NewLine + "Exception message: " + ex.Message);
             }
         }
@@ -44,6 +45,35 @@ namespace DKSuccess {
 
         public static bool operator >(Death d1, Death d2) {
             return (d1.Level > d2.Level || (d1.Level == d2.Level && d1.Board > d2.Board));
+        }
+
+        // Check if a death fulfils the criteria given
+        public bool IsType(int minLevel, int maxLevel, char boardType = 'A') {
+            if (Level < minLevel || Level > maxLevel)
+                return (false);
+
+            if (boardType == 'A' || GetBoardType() == boardType)
+                return (true);
+            else
+                return (false);
+        }
+
+
+        // is this death on a [B]arrel, [S]pring, [C]onveyor or [R]ivet board?
+        public char GetBoardType() {
+            // from level five on, the pattern repeats
+            int lvl = Math.Min(5, Level);
+
+            if (Board == lvl + 1)
+                return 'R';
+            else if (Board == 2)
+                return (Level == 2 ? 'S' : 'C');
+            else if (Board == 3)
+                return (Level == 3 ? 'S' : 'B');
+            else if (Board == 4)
+                return ('S');
+            else
+                return ('B');
         }
     }
 }
